e8a4a9090cd7df02084aee4d5eb45ddba1546eb7
matheuz233 <matheusaugus233@gmail.com>
2025-11-26 16:02:43 -0300

    feat: :lock: Adiciona autenticação por senha na rota de trigger

--- app/routers/analysis_route.py
+++ app/routers/analysis_route.py
@@ -1,14 +1,23 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter, HTTPException
 from app.utils.state_utils import get_running, set_running
 from app.controller.analysis_controller import analysis
+from pydantic import BaseModel
 
 import asyncio
 
-# router = APIRouter(prefix="/classify", tags=["Classification"])
+TRIGGER_PASSWORD = os.getenv("TRIGGER_PASSWORD")
+
+class TriggerRequest(BaseModel):
+    password: str
+
 router = APIRouter(prefix='/analysis', tags=["Analysis"])
 
 @router.post("/trigger")
-async def trigger():
+async def trigger(payload: TriggerRequest):
+    if payload.password != TRIGGER_PASSWORD:
+        raise HTTPException(status_code=401, detail="Unauthorized: wrong password")
+
     if get_running() == False:
         set_running(True)
         asyncio.create_task(asyncio.to_thread(analysis))
